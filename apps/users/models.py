be3fb018b97feaf68cf3e3499de6b443a341e527
Toktorov <ktoktorov2005@gmail.com>
2021-06-13 10:21:17 +0600

    fix problems with images in index

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'profile')
     nickname = models.CharField(max_length=255)
     image = models.ImageField(upload_to='profile')
 
